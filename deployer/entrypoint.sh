2729fe28c7097cfcbd0cb87561de9f91ba6ae7df
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-06-10 15:10:14 -0700

    Create namespace if it doesn't exist

--- deployer/entrypoint.sh
+++ deployer/entrypoint.sh
@@ -5,5 +5,7 @@ set -exuo pipefail
 envsubst < /data/manifests/application/application.yaml.tmpl > /data/manifests/application/application.yaml
 kubectl apply -f /data/manifests/application/
 
+kubectl get namespace $NAMESPACE || kubectl create namespace $NAMESPACE
+
 envsubst < /data/manifests/kustomize/kustomization.yaml.tmpl > /data/manifests/kustomize/kustomization.yaml
 kustomize build /data/manifests/kustomize | kubectl apply -f -
