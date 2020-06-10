81f55feaff5a9ae76381a1325c2bffb724f7ca33
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-06-10 16:24:43 -0700

    Move kustomize resources out of the way

--- deployer/entrypoint.sh
+++ deployer/entrypoint.sh
@@ -7,5 +7,6 @@ kubectl get namespace $NAMESPACE || kubectl create namespace $NAMESPACE
 envsubst < /data/manifest/application/application.yaml.tmpl > /data/manifest/application/application.yaml
 envsubst < /data/manifest/kustomize/kustomization.yaml.tmpl > /data/manifest/kustomize/kustomization.yaml
 cp /data/manifest/application/*.yaml /data/manifest/kustomize/*.yaml /data/
+rm -rf /data/manifest/application/ /data/manifest/kustomize/
 
 /bin/deploy.sh
